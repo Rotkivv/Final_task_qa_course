9b81588138e4ae57bdacadc35f0eb0c631a093f1
TVicT <Victorupsi25@gmail.com>
2019-07-13 16:32:26 +0300

    added login page and verefication of registration form and login form on it

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+class LoginPageLocators(object):
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") 
+    REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-import time
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):  
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
@@ -8,3 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page.open() 
     page.should_be_login_link()
     page.go_to_login_page()
+    
+    login_page = LoginPage(browser, "http://selenium1py.pythonanywhere.com/ru/accounts/login/")
+    login_page.open()
+    login_page.should_be_login_page()

005ac70c82334db1473113dec7a0e8940d23e47c
TVicT <Victorupsi25@gmail.com>
2019-07-17 18:41:09 +0300

    added two methods to basa_page and new locators for basket page

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini>span>a")
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
@@ -18,3 +19,7 @@ class ProductPageLocators(object):
 
     ALERT_SUCCESS_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages>.alert-success:nth-child(1)>.alertinner > strong')
     ALERT_INFO_TOTAL_BACKET = (By.CSS_SELECTOR, '#messages > .alert-info strong')
+
+class BasketPageLocators(object):
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner>p")
+    BASKET_SUMMARY = (By.CSS_SELECTOR,".basket_summary")

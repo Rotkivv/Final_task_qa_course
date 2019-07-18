76d2277ddfe84f1b1d9ad802fa7f7331c6cfa048
TVicT <Victorupsi25@gmail.com>
2019-07-18 19:47:11 +0300

    added two new tests for registred users: test_user_cant_see_success_message, test_user_can_add_product_to_cart

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import  LoginPage
 import time
 import pytest
 
@@ -83,3 +84,39 @@ def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     page.go_to_basket_page()
     page.should_not_be_see_product_in_basket()
     page.should_be_message_basket_is_empty()
+
+class TestUserAddToCartFromProductPage(object):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        
+        page = LoginPage(browser, link)
+        page.open()
+
+        page.register_new_user('test'+str(time.time()) + "@fakemail.org", 'Qqwertyuiop')
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        
+        product_page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_cart(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    
+        product_page = ProductPage(browser, link)
+        product_page.open()
+
+        product_page.should_be_product_name()
+        product_page.should_be_product_price()
+        product_page.should_be_button_add_to_basket()
+        product_page.should_not_be_success_message()
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.should_be_message_added_to_basket()
+        product_page.should_be_successful_message_conteins_product_name()
+        product_page.should_be_alert_info_message_basket_total()
+        product_page.should_be_basket_total_matches_product_price()
